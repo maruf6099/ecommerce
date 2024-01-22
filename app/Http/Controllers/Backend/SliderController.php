f8112ffe62b917edf015d08ff73cb563fe3c19d4
maruf <almaruf609@gmail.com>
2024-01-22 22:10:45 +0600

    remove extra header SliderController

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
-use App\Traits\ImageUploadTraits;
+
 use App\Traits\ImageUploadTrait;
 
 class SliderController extends Controller
